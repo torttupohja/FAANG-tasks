98d6fd9bfcdf0662f31cf3ebf8453fb601a5cf8f
torttupohja <84392573+torttupohja@users.noreply.github.com>
2021-06-13 16:07:10 +0100

    Delete test/models directory

--- test/models/ruby_tests.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ArticleTest < ActiveSupport::TestCase
-  	test "should not save article without title" do
-  		article = Article.new
-  		assert_not article.save
-	end
-end
