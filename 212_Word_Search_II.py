4fbcdb1bd83e21f57c5da4a17afca7f6255af30f
torttupohja <84392573+torttupohja@users.noreply.github.com>
2025-07-06 18:33:58 -0400

    Create 212_Word_Search_II.py

---
+++ 212_Word_Search_II.py
@@ -0,0 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None  # Stores full word at end node for quick result collection
+
+def build_trie(words):
+    root = TrieNode()
+    for word in words:
+        node = root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.word = word  # Store word at the end node
+    return root
+
+def findWords(board, words):
+    rows, cols = len(board), len(board[0])
+    result = []
+    root = build_trie(words)
+
+    def dfs(r, c, node):
+        char = board[r][c]
+        if char not in node.children:
+            return
+
+        next_node = node.children[char]
+        if next_node.word:
+            result.append(next_node.word)
+            next_node.word = None  # Avoid duplicates
+
+        # Mark visited
+        board[r][c] = '#'
+
+        for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] != '#':
+                dfs(nr, nc, next_node)
+
+        board[r][c] = char  # Restore original character
+
+        # Optional pruning: remove leaf nodes
+        if not next_node.children:
+            node.children.pop(char)
+
+    for r in range(rows):
+        for c in range(cols):
+            dfs(r, c, root)
+
+    return result
