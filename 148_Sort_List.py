4858941338870af861a502c016d58c88c8c295c0
torttupohja <84392573+torttupohja@users.noreply.github.com>
2025-08-17 17:50:27 -0400

    solution for 148_Sort_List.py

---
+++ 148_Sort_List.py
@@ -0,0 +1,34 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def sortList(head):
+    if not head or not head.next:
+        return head
+
+    # Split list into two halves
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    mid = slow.next
+    slow.next = None
+
+    left = sortList(head)
+    right = sortList(mid)
+
+    return merge(left, right)
+
+def merge(l1, l2):
+    dummy = ListNode()
+    tail = dummy
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next, l1 = l1, l1.next
+        else:
+            tail.next, l2 = l2, l2.next
+        tail = tail.next
+    tail.next = l1 or l2
+    return dummy.next
