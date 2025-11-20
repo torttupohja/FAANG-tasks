ebe90d5d7e904739bca13a3460daf6ee6a275a84
torttupohja <84392573+torttupohja@users.noreply.github.com>
2025-11-20 18:22:45 -0500

    solution for 162_Find_Peak_Element.py

---
+++ 162_Find_Peak_Element.py
@@ -0,0 +1,14 @@
+def find_peak_element(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return left
+
+"""
+Time complexity: O(log n)
+Space complexity: O(1)
+"""
