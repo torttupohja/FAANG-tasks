9ba2951439c1e0d37bb4ded475f4258ead63bb07
torttupohja <84392573+torttupohja@users.noreply.github.com>
2026-02-01 09:29:38 -0500

    Solution for 153_Find_Minimum_in_Rotated_Sorted_Array.py

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,14 @@
+def find_min(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return nums[left]
+
+"""
+Time complexity: O(log n)
+Space complexity: O(1)
+"""
